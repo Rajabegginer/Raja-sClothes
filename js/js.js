2f9c26bb9fc742eaf3020221d007cce06c7f8323
Raja Oad <103353840+Rajabegginer@users.noreply.github.com>
2022-09-04 18:38:27 +0500

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,13 @@
+
+
+function navon(){
+    document.getElementById('navbar').style.display = "block";
+    document.getElementById('ham1').style.display = "none";
+    document.getElementById('ham2').style.display = "inline";
+}
+
+function navoff(){
+    document.getElementById('navbar').style.display = "none";
+    document.getElementById('ham2').style.display = "none";
+    document.getElementById('ham1').style.display = "inline";
+}
